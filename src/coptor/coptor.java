51e4acadf5b22000510b4d54c6fcf2392fff9a28
AMRUDESH PS <126590195+AMRUDESHPS@users.noreply.github.com>
2024-09-18 20:06:25 +0530

    java coptor animation JAVA

---
+++ src/coptor/coptor.java
@@ -0,0 +1,91 @@
+package coptor;
+
+public class coptor {
+
+	public static void main(String[] args) throws InterruptedException {
+		int count = 1;
+		
+		while(true) {
+			if(count == 1) {
+				System.out.println("           OL:ROFL:ROFL     ");
+				System.out.println("           A            ");
+				System.out.println("       /------------                 ");
+				System.out.println("LOL====         [😁] \\               ");
+				System.out.println(" L    \\               \\     ");
+				System.out.println("       \\______________/  ");
+				System.out.println("       ______I_____I_______/     ");
+				count = 0;
+				
+				Thread.sleep(100);
+				
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				
+				System.out.println("ROFL:ROFL:LO                ");
+				System.out.println("           A            ");
+				System.out.println(" L     /------------                 ");
+				System.out.println(" OL====         [😁] \\               ");
+				System.out.println(" L    \\               \\     ");
+				System.out.println("       \\______________/  ");
+				System.out.println("       ______I_____I_______/     ");
+				
+				Thread.sleep(100);
+				
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+			}else {
+
+				System.out.println("           OL:ROFL:ROFL     ");
+				System.out.println("           A            ");
+				System.out.println(" L     /------------                 ");
+				System.out.println("LOL====         [😁] \\               ");
+				System.out.println("      \\               \\     ");
+				System.out.println("       \\______________/  ");
+				System.out.println("       ______I_____I_______/     ");
+				
+				Thread.sleep(100);
+				
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+
+				System.out.println("ROFL:ROFL:LO                 ");
+				System.out.println("           A            ");
+				System.out.println(" L     /------------                 ");
+				System.out.println("LO ====         [😁] \\               ");
+				System.out.println(" L    \\               \\     ");
+				System.out.println("       \\______________/  ");
+				System.out.println("       ______I_____I_______/     ");
+				count = 1;
+				
+				Thread.sleep(100);
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+		}
+	}
+
+}
